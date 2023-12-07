4e484d33802771cc18cca2d230336f4e6dd2b7da
Klebertt <126410502+Klebertt@users.noreply.github.com>
2023-12-07 16:50:22 -0300

    Update exercicio_com_arquivos.py

--- exercicio_com_arquivos.py
+++ exercicio_com_arquivos.py
@@ -15,14 +15,30 @@ def form():
         sexo = input('Qual o seu sexo?')
         arquivo.write(sexo)
         arquivo.write("|")
-        
+
         telefone = input('Qual o seu telefone?')
         arquivo.write(telefone)
         arquivo.write('\n')
         arquivo.close()
 
-form()
-
-def ler():
+def ler_dados():
     arquivo = open('meu_arquivo.txt', 'r', encoding='utf-8')
-    print(arquivo.read())
+    linhas = arquivo.readlines()
+    arquivo.close()
+
+    for linha in linhas:
+        dados = linha.strip().split('|')
+
+        nome = dados[0]
+        idade = dados[1] + " anos"
+        sexo = "Masculino" if dados[2] == 'M' else "Feminino"
+        telefone = dados[3]
+
+        print(f"• Nome: {nome}")
+        print(f"• Idade: {idade}")
+        print(f"• Sexo: {sexo}")
+        print(f"• Telefone: {telefone}")
+        print()
+
+form()
+ler_dados()
